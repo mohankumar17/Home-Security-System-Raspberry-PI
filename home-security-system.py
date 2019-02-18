a6339b6e58fdc296b632c7b14bde50473b2d0af9
Mohan Kumar <33448410+mohankumar17@users.noreply.github.com>
2019-02-18 19:58:39 +0530

    Update home-security-system.py

--- home-security-system.py
+++ home-security-system.py
@@ -1,5 +1,5 @@
 # raspberry-based-home-security
-This is the code for my project on home security sytem using raspberry pi 3 model B
+
 'import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
